6572cace61506cdf1276bb7c7a6efa690a5f61c2
veenapilli <veenapilli@gmail.com>
2019-03-11 12:56:59 +1100

    initial commit

---
+++ app/src/main/java/com/veenapilli/learningthroughoxford/LandingActivity.kt
@@ -0,0 +1,12 @@
+package com.veenapilli.learningthroughoxford
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class LandingActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_landing)
+    }
+}
